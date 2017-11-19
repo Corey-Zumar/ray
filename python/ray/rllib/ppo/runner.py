96e3071f5421d0eb241b73f49ec69234254c570e
Corey Zumar <czumar@berkeley.edu>
2017-11-18 21:00:27 -0800

    fix

--- python/ray/rllib/ppo/runner.py
+++ python/ray/rllib/ppo/runner.py
@@ -147,16 +147,8 @@ class Runner(object):
             raise Exception("Unknown observation_filter: " +
                             str(config["observation_filter"]))
         self.reward_filter = NoFilter()
+        self.sess.run(self.variables.initializer)
 
-        def initialize_uninitialized(sess):
-            global_vars          = tf.global_variables()
-            is_not_initialized   = sess.run([tf.is_variable_initialized(var) for var in global_vars])
-            not_initialized_vars = [v for (v, f) in zip(global_vars, is_not_initialized) if not f]
-            
-            if len(not_initialized_vars):
-                sess.run(tf.variables_initializer(not_initialized_vars))
-
-        initialize_uninitialized(self.sess)
 
     def load_data(self, trajectories, full_trace):
         if self.config["use_gae"]:
