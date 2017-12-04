dea599b8bdb35c428e6caf8ed553930c8f7f1fc6
Eric Liang <ekhliang@gmail.com>
2017-12-03 16:40:05 -0800

    improve with vision net

--- python/ray/rllib/models/visionnet.py
+++ python/ray/rllib/models/visionnet.py
@@ -6,12 +6,14 @@ import tensorflow as tf
 import tensorflow.contrib.slim as slim
 
 from ray.rllib.models.model import Model
+from ray.rllib.models.misc import normc_initializer
 
 
 class VisionNetwork(Model):
     """Generic vision network."""
 
     def _init(self, inputs, num_outputs, options):
+        extra = options.get("extra_inputs")
         filters = options.get("conv_filters", [
             [16, [8, 8], 4],
             [32, [4, 4], 2],
@@ -25,6 +27,19 @@ class VisionNetwork(Model):
             out_size, kernel, stride = filters[-1]
             fc1 = slim.conv2d(
                 inputs, out_size, kernel, stride, padding="VALID", scope="fc1")
-            fc2 = slim.conv2d(fc1, num_outputs, [1, 1], activation_fn=None,
-                              normalizer_fn=None, scope="fc2")
-            return tf.squeeze(fc2, [1, 2]), tf.squeeze(fc1, [1, 2])
+            if extra is None:
+                fc2 = slim.conv2d(fc1, num_outputs, [1, 1], activation_fn=None,
+                                  normalizer_fn=None, scope="fc2")
+                out = tf.squeeze(fc2, [1, 2])
+            else:
+                fc1_aug = tf.concat([tf.squeeze(fc1, [1, 2]), extra], axis=1)
+                fc2 = slim.fully_connected(
+                    fc1_aug, num_outputs,
+                    weights_initializer=normc_initializer(1.0),
+                    activation_fn=tf.nn.tanh, scope="fc2_aug")
+                fc2_aug = tf.concat([fc2, extra], axis=1)
+                out = slim.fully_connected(
+                    fc2_aug, num_outputs,
+                    weights_initializer=normc_initializer(0.01),
+                    activation_fn=None, scope="fc_out_aug")
+            return out, tf.squeeze(fc1, [1, 2])
